8e10470aafe1b1d2a4334464a45ec5aaaf8e124c
LFISHER7 <lhf282@live.co.uk>
2021-02-09 17:01:09 +0000

    fix graph for whole population

--- analysis/generate_plots.py
+++ analysis/generate_plots.py
@@ -2,18 +2,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-measures_df_sex = pd.read_csv('../output/measures/measure_pulse_ox_by_sex.csv')
+measures_df_sex = pd.read_csv('output/measures/measure_pulse_ox_by_sex.csv')
 measures_df_region = pd.read_csv(
-    '../output/measures/measure_pulse_ox_by_region.csv')
+    'output/measures/measure_pulse_ox_by_region.csv')
 measures_df_age = pd.read_csv(
-    '../output/measures/measure_pulse_ox_by_age_band.csv')
+    'output/measures/measure_pulse_ox_by_age_band.csv')
 measures_df_total = pd.read_csv(
-    '../output/measures/measure_pulse_ox_total.csv')
+    'output/measures/measure_pulse_ox_total.csv')
 
 
 #temporary fix for population not working in Measures
-# measures_df_total = measures_df_total.groupby(
-#     ['date'])['had_pulse_ox', 'population', 'value'].sum().reset_index()
+measures_df_total = measures_df_total.groupby(
+    ['date'])['had_pulse_ox', 'population'].sum().reset_index()
+measures_df_total['value'] = measures_df_total['had_pulse_ox'] / \
+    measures_df_total['population']
 
 
 def to_datetime_sort(df):
@@ -24,20 +26,32 @@ def to_datetime_sort(df):
 to_datetime_sort(measures_df_sex)
 to_datetime_sort(measures_df_region)
 to_datetime_sort(measures_df_age)
+to_datetime_sort(measures_df_total)
 
 
-def plot_measures(df, title, filename, category=False):
+def calculate_rate(df, value_col='had_pulse_ox', population_col='population'):
+    num_per_hundred_thousand = df[value_col]/(df[population_col]/100000)
+    df['num_per_hundred_thousand'] = num_per_hundred_thousand
+
+
+calculate_rate(measures_df_sex)
+calculate_rate(measures_df_age)
+calculate_rate(measures_df_region)
+calculate_rate(measures_df_total)
+
+
+def plot_measures(df, title, filename, column_to_plot, category=False, y_label='Number per 100, 000'):
 
     if category:
         for unique_category in df[category].unique():
 
             df_subset = df[df[category] == unique_category]
 
-            plt.plot(df_subset['date'], df_subset['value'])
+            plt.plot(df_subset['date'], df_subset[column_to_plot])
     else:
-        plt.plot(df['date'], df['value'])
+        plt.plot(df['date'], df[column_to_plot])
 
-    plt.ylabel('% of population')
+    plt.ylabel(y_label)
     plt.xlabel('Date')
     plt.xticks(rotation='vertical')
     plt.title(title)
@@ -49,15 +63,17 @@ def plot_measures(df, title, filename, category=False):
     else:
         pass
 
-    plt.savefig(f'../output/figures/{filename}.jpeg', bbox_inches='tight')
-    plt.show()
+    plt.savefig(f'output/figures/{filename}.jpeg', bbox_inches='tight')
+   
+    plt.clf()
 
 
-plot_measures(measures_df_sex, 'Pulse Oximetry use by Sex',
-              'sex_rates', 'sex', )
-plot_measures(measures_df_region, 'Pulse Oximetry use by Region',
-              'region_rates', 'region')
-plot_measures(measures_df_age, 'Pulse Oximetry use by Age Band',
-              'age_rates', 'age_band')
-plot_measures(measures_df_total, 'Pulse Oximetry us across Whole Population',
-              'population_rates', category=False)
+plot_measures(measures_df_total, 'Pulse Oximetry use across Whole Population',
+              'population_rates', 'had_pulse_ox', category=False, y_label='Total Number')
+
+plot_measures(measures_df_sex,
+              'Pulse Oximetry use by Sex per 100, 000', 'sex_rates', 'num_per_hundred_thousand', category='sex', )
+plot_measures(measures_df_region,
+              'Pulse Oximetry use by Region per 100, 000', 'region_rates', 'num_per_hundred_thousand', category='region')
+plot_measures(measures_df_age,
+              'Pulse Oximetry use by Age Band per 100, 000',  'age_rates', 'num_per_hundred_thousand', category='age_band')
